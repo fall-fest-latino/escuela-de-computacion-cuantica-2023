ba0facc2e30ba52c10ebbb38cf3032bbf1e1edee
Yessi <ayge.1904@gmail.com>
2023-11-08 21:28:53 -0600

    agregando fotos

--- hackathon/main.py
+++ hackathon/main.py
@@ -2,5 +2,60 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-imageLocal = cv2.imread("bancos/COVID/1.jpg",cv2.IMREAD_GRAYSCALE)
-print(imageLocal.shape)
+# imageLocal = cv2.imread("bancos/COVID/1.jpg",cv2.IMREAD_GRAYSCALE)
+# print(imageLocal.shape)
+import torch
+from torch import cat, no_grad, manual_seed
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import torch.optim as optim
+from torch.nn import (
+    Module,
+    Conv2d,
+    Linear,
+    Dropout2d,
+    NLLLoss,
+    MaxPool2d,
+    Flatten,
+    Sequential,
+    ReLU,
+)
+import torch.nn.functional as F
+
+
+manual_seed(42)
+
+batch_size = 1
+n_samples = 100  # We will concentrate on the first 100 samples
+
+# Use pre-defined torchvision function to load MNIST train data
+X_train = datasets.MNIST(
+    root="./data", train=True, download=True, transform=transforms.Compose([transforms.ToTensor()])
+)
+
+# Filter out labels (originally 0-9), leaving only labels 0 and 1
+idx = np.append(
+    np.where(X_train.targets == 0)[0][:n_samples], np.where(X_train.targets == 1)[0][:n_samples]
+)
+X_train.data = X_train.data[idx]
+X_train.targets = X_train.targets[idx]
+
+# # Define torch dataloader with filtered data
+train_loader = DataLoader(X_train, batch_size=batch_size, shuffle=True)
+
+
+
+n_samples_show = 6
+
+data_iter = iter(train_loader)
+fig, axes = plt.subplots(nrows=1, ncols=n_samples_show, figsize=(10, 3))
+
+while n_samples_show > 0:
+    images, targets = data_iter.__next__()
+
+    axes[n_samples_show - 1].imshow(images[0, 0].numpy().squeeze(), cmap="gray")
+    axes[n_samples_show - 1].set_xticks([])
+    axes[n_samples_show - 1].set_yticks([])
+    axes[n_samples_show - 1].set_title("Labeled: {}".format(targets[0].item()))
+
+    n_samples_show -= 1

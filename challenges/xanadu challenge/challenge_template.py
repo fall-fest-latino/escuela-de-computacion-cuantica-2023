0b3b9c44dbe5e95d17c0f62a119c7a42e4d83bdd
Fátima Quijas Escalera <54221717+FatimaQuiEsc@users.noreply.github.com>
2023-11-09 11:00:39 -0600

    Update challenge_template.py

--- challenges/xanadu challenge/challenge_template.py
+++ challenges/xanadu challenge/challenge_template.py
@@ -2,61 +2,61 @@
 
 import pennylane as qml
 from pennylane import numpy as np
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
 
 WIRES = 2
 LAYERS = 5
 NUM_PARAMETERS = LAYERS * WIRES * 3
 
 def variational_circuit(params,hamiltonian):
-    """
-    This is a template variational quantum circuit containing a fixed layout of gates with variable
-    parameters. To be used as a QNode, it must either be wrapped with the @qml.qnode decorator or
-    converted using the qml.QNode function.
-
-    The output of this circuit is the expectation value of a Hamiltonian, somehow encoded in
-    the hamiltonian argument
-
-    Args:
-        - params (np.ndarray): An array of optimizable parameters of shape (30,)
-        - hamiltonian (np.ndarray): An array of real parameters encoding the Hamiltonian
-        whose expectation value is returned.
-    
-    Returns:
-        (float): The expectation value of the Hamiltonian
-    """
     parameters = params.reshape((LAYERS, WIRES, 3))
     qml.templates.StronglyEntanglingLayers(parameters, wires=range(WIRES))
     return qml.expval(qml.Hermitian(hamiltonian, wires = [0,1]))
-
 def optimize_circuit(hamiltonian):
-    """Minimize the variational circuit and return its minimum value.
-    You should create a device and convert the variational_circuit function 
-    into an executable QNode. 
-    Next, you should minimize the variational circuit using gradient-based 
-    optimization to update the input params. 
-    Return the optimized value of the QNode as a single floating-point number.
-
-    Args:
-        - params (np.ndarray): Input parameters to be optimized, of dimension 30
-        - hamiltonian (np.ndarray): An array of real parameters encoding the Hamiltonian
-        whose expectation value you should minimize.
-    Returns:
-        float: the value of the optimized QNode
-    """
-        
     hamiltonian = np.array(hamiltonian, requires_grad = False)
-
     hamiltonian = np.array(hamiltonian,float).reshape((2 ** WIRES), (2 ** WIRES))
 
-    ### WRITE YOUR CODE BELOW THIS LINE
-    
+    ### WRITE YOUR CODE BELOW THIS LINE    
     ### Solution Template
+    dev = qml.device("default.qubit", wires = WIRES)# Initialize the device.
+    
+    w = qml.numpy.random.rand(NUM_PARAMETERS)
+#    qnod = qml.QNode(variational_circuit(w,hamiltonian),dev)
+    @qml.qnode(dev)
+    def cost(wei):
+        return variational_circuit(wei,hamiltonian)
+        
+# Write your code to minimize the circuit
+    opt=qml.GradientDescentOptimizer(stepsize=0.3)  
+    
+# set the number of steps
+    steps = 160
+    
+# set the initial parameter values
+    params = w
 
-    dev = # Initialize the device.
+    for i in tqdm(range(steps)):
+        # update the circuit parameters
+        params = opt.step(cost, params)
 
-    circuit = # Instantiate the QNode from variational_circuit.
+        if (i + 1) % 10 == 0:
+            print("Cost after step {:5d}: {: .8f}".format(i + 1, cost(params)))
 
-    # Write your code to minimize the circuit
 
-    return # Return the value of the minimized QNode
+in1=np.array([0.863327072347624,0.0167108057202516,0.07991447085492759,0.0854049026262154,
+              0.0167108057202516,0.8237963773906136,-0.07695947154193797,0.03131548733285282,
+              0.07991447085492759,-0.07695947154193795,0.8355417021014687,-0.11345916130631205,
+              0.08540490262621539,0.03131548733285283,-0.11345916130631205,0.758156886827099])
+#Expected output: 0.61745341
+in2=np.array([0.32158897156285354,-0.20689268438270836,0.12366748295758379,-0.11737425017261123,
+              -0.20689268438270836,0.7747346055276305,-0.05159966365446514,0.08215539696259792,
+              0.12366748295758379,-0.05159966365446514,0.5769050487087416,0.3853362904758938,
+              -0.11737425017261123,0.08215539696259792,0.3853362904758938,0.3986256655167206])
+#Expected output: 0.00246488
 
+
+#Run test cases
+optimize_circuit(in1)
+
+optimize_circuit(in2)
